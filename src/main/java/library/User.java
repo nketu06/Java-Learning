738e18ee892ac04f08b0005725f5be83cd3b51c3
Nishant ketu <nketu06@gmail.com>
2024-12-11 21:39:19 +0530

    added User

---
+++ src/main/java/library/User.java
@@ -0,0 +1,15 @@
+package library;
+
+public class User {
+    private String name;
+    private int userId;
+    private String ContactInfo;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContactInfo() {
+        return ContactInfo;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+
+
+        }
+}
+
